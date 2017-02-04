af368799f52dd3142289ea3cee58c4f930196797
sagarssabat <sagarssabat@gmail.com>
2017-02-04 19:04:29 +0530

    popup for signup and login done

--- frontend/js/controllers.js
+++ frontend/js/controllers.js
@@ -1,73 +1,74 @@
 angular.module('phonecatControllers', ['templateservicemod', 'navigationservice', 'ui.bootstrap', 'ngAnimate', 'ngSanitize', 'angular-flexslider', 'ui.swiper'])
 
-.controller('HomeCtrl', function ($scope, TemplateService, NavigationService, $timeout, $uibModal) {
-    $scope.template = TemplateService.changecontent("home"); //Use same name of .html file
-    $scope.menutitle = NavigationService.makeactive("Home"); //This is the Title of the Website
-    TemplateService.title = $scope.menutitle;
-    $scope.navigation = NavigationService.getnav();
+    .controller('HomeCtrl', function ($scope, TemplateService, NavigationService, $timeout, $uibModal) {
+        $scope.template = TemplateService.changecontent("home"); //Use same name of .html file
+        $scope.menutitle = NavigationService.makeactive("Home"); //This is the Title of the Website
+        TemplateService.title = $scope.menutitle;
+        $scope.navigation = NavigationService.getnav();
 
-    $scope.mySlides = [
-        'frontend/img/banner/bg-home.jpg',
-        'frontend/img/banner/bg-home.jpg',
-        'frontend/img/banner/bg-home.jpg',
-        'frontend/img/banner/bg-home.jpg'
-    ];
+        $scope.mySlides = [
+            'frontend/img/banner/bg-home.jpg',
+            'frontend/img/banner/bg-home.jpg',
+            'frontend/img/banner/bg-home.jpg',
+            'frontend/img/banner/bg-home.jpg'
+        ];
 
-})
+    })
 
-.controller('FormCtrl', function ($scope, TemplateService, NavigationService, $timeout) {
-    $scope.template = TemplateService.changecontent("form"); //Use same name of .html file
-    $scope.menutitle = NavigationService.makeactive("Form"); //This is the Title of the Website
-    TemplateService.title = $scope.menutitle;
-    $scope.navigation = NavigationService.getnav();
+    .controller('FormCtrl', function ($scope, TemplateService, NavigationService, $timeout) {
+        $scope.template = TemplateService.changecontent("form"); //Use same name of .html file
+        $scope.menutitle = NavigationService.makeactive("Form"); //This is the Title of the Website
+        TemplateService.title = $scope.menutitle;
+        $scope.navigation = NavigationService.getnav();
 
-    $scope.formSubmitted = false;
+        $scope.formSubmitted = false;
 
-    $scope.submitForm = function (data) {
-        console.log(data);
-        $scope.formSubmitted = true;
-    }
-})
+        $scope.submitForm = function (data) {
+            console.log(data);
+            $scope.formSubmitted = true;
+        }
+    })
 
-.controller('headerctrl', function ($scope, TemplateService, $uibModal) {
-    $scope.template = TemplateService;
-    $scope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
-        $(window).scrollTop(0);
-    });
-    $.fancybox.close(true);
-    console.log("in header");
-    $scope.openSignupModal = function () {
-        console.log("clla");
-        $uibModal.open({
-            animation: true,
-            templateUrl: 'frontend/views/modal/signup.html',
-            scope: $scope,
-            size: 'lg'
+    .controller('headerctrl', function ($scope, TemplateService, $uibModal) {
+        $scope.template = TemplateService;
+        $scope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
+            $(window).scrollTop(0);
         });
-    };
-})
+        $.fancybox.close(true);
+        console.log("in header");
+        $scope.openSignupModal = function () {
+            console.log("clla");
+            $uibModal.open({
+                animation: true,
+                templateUrl: 'frontend/views/modal/signup.html',
+                scope: $scope,
+                size: 'lg',
+                windowClass: 'modal-content-radi0'
+            });
+        };
+    })
 
-.controller('languageCtrl', function ($scope, TemplateService, $translate, $rootScope) {
+    .controller('languageCtrl', function ($scope, TemplateService, $translate, $rootScope) {
 
-    $scope.changeLanguage = function () {
-        console.log("Language CLicked");
+        $scope.changeLanguage = function () {
+            console.log("Language CLicked");
 
-        if (!$.jStorage.get("language")) {
-            $translate.use("hi");
-            $.jStorage.set("language", "hi");
-        } else {
-            if ($.jStorage.get("language") == "en") {
+            if (!$.jStorage.get("language")) {
                 $translate.use("hi");
                 $.jStorage.set("language", "hi");
             } else {
-                $translate.use("en");
-                $.jStorage.set("language", "en");
+                if ($.jStorage.get("language") == "en") {
+                    $translate.use("hi");
+                    $.jStorage.set("language", "hi");
+                } else {
+                    $translate.use("en");
+                    $.jStorage.set("language", "en");
+                }
             }
-        }
-        //  $rootScope.$apply();
-    };
+            //  $rootScope.$apply();
+        };
 
 
-})
+    })
 
-;
+    ;
