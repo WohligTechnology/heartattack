c0942b094d0f2a648cd8c12d92c4e9bec9a59c89
sagarssabat <sagarssabat@gmail.com>
2017-02-24 19:15:44 +0530

    myaccount changes done

--- frontend/js/controllers.js
+++ frontend/js/controllers.js
@@ -1,21 +1,21 @@
 angular.module('phonecatControllers', ['templateservicemod', 'navigationservice', 'ui.bootstrap', 'ngAnimate', 'ngSanitize', 'angular-flexslider', 'ui.swiper'])
 
-    .controller('HomeCtrl', function ($scope, TemplateService, NavigationService, $timeout, $uibModal) {
-        $scope.template = TemplateService.changecontent("home"); //Use same name of .html file
-        $scope.menutitle = NavigationService.makeactive("Home"); //This is the Title of the Website
-        TemplateService.title = $scope.menutitle;
-        $scope.navigation = NavigationService.getnav();
+.controller('HomeCtrl', function ($scope, TemplateService, NavigationService, $timeout, $uibModal) {
+    $scope.template = TemplateService.changecontent("home"); //Use same name of .html file
+    $scope.menutitle = NavigationService.makeactive("Home"); //This is the Title of the Website
+    TemplateService.title = $scope.menutitle;
+    $scope.navigation = NavigationService.getnav();
 
-        $scope.mySlides = [
-            'frontend/img/banner/bg-home.jpg',
-            'frontend/img/banner/bg-home.jpg',
-            'frontend/img/banner/bg-home.jpg',
-            'frontend/img/banner/bg-home.jpg'
-        ];
+    $scope.mySlides = [
+        'frontend/img/banner/bg-home.jpg',
+        'frontend/img/banner/bg-home.jpg',
+        'frontend/img/banner/bg-home.jpg',
+        'frontend/img/banner/bg-home.jpg'
+    ];
 
-    })
+})
 
-    .controller('FormCtrl', function ($scope, TemplateService, NavigationService, $timeout) {
+.controller('FormCtrl', function ($scope, TemplateService, NavigationService, $timeout) {
         $scope.template = TemplateService.changecontent("form"); //Use same name of .html file
         $scope.menutitle = NavigationService.makeactive("Form"); //This is the Title of the Website
         TemplateService.title = $scope.menutitle;
@@ -64,15 +64,15 @@ angular.module('phonecatControllers', ['templateservicemod', 'navigationservice'
         console.log('$scope.myArray ', $scope.myArray);
 
         $scope.imgFootscetion = [{
-            image: "frontend/img/orientation/switch-img-1.jpg",
-            thumbnail: "advanced"
-        }, {
-            image: "frontend/img/orientation/switch-img-2.jpg",
-            thumbnail: "cardiothoracic icu rotation"
-        }, {
-            image: "frontend/img/orientation/switch-img-3.jpg",
-            thumbnail: "european diploma in intensice care preparation pack"
-        }
+                image: "frontend/img/orientation/switch-img-1.jpg",
+                thumbnail: "advanced"
+            }, {
+                image: "frontend/img/orientation/switch-img-2.jpg",
+                thumbnail: "cardiothoracic icu rotation"
+            }, {
+                image: "frontend/img/orientation/switch-img-3.jpg",
+                thumbnail: "european diploma in intensice care preparation pack"
+            }
             //  {
             //     image: "frontend/img/orientation/switch-img-1.jpg",
             //     thumbnail: "image2"
@@ -181,6 +181,18 @@ angular.module('phonecatControllers', ['templateservicemod', 'navigationservice'
             description: "Orientation - CT Surgery",
             price: "49.99$",
             data: "20 Oct 2016"
+        }, {
+            serial: "04",
+            image: "frontend/img/myaccount/airway-img-3.png",
+            description: "Cardiothoracic ICU Rotation - Generic",
+            price: "49.99$",
+            data: "20 Oct 2016"
+        }, {
+            serial: "05",
+            image: "frontend/img/myaccount/airway-img-1.png",
+            description: "Orientation - CT Surgery",
+            price: "49.99$",
+            data: "20 Oct 2016"
         }]
 
     })
@@ -270,27 +282,27 @@ angular.module('phonecatControllers', ['templateservicemod', 'navigationservice'
         };
     })
 
-    .controller('languageCtrl', function ($scope, TemplateService, $translate, $rootScope) {
+.controller('languageCtrl', function ($scope, TemplateService, $translate, $rootScope) {
 
-        $scope.changeLanguage = function () {
-            console.log("Language CLicked");
+    $scope.changeLanguage = function () {
+        console.log("Language CLicked");
 
-            if (!$.jStorage.get("language")) {
+        if (!$.jStorage.get("language")) {
+            $translate.use("hi");
+            $.jStorage.set("language", "hi");
+        } else {
+            if ($.jStorage.get("language") == "en") {
                 $translate.use("hi");
                 $.jStorage.set("language", "hi");
             } else {
-                if ($.jStorage.get("language") == "en") {
-                    $translate.use("hi");
-                    $.jStorage.set("language", "hi");
-                } else {
-                    $translate.use("en");
-                    $.jStorage.set("language", "en");
-                }
+                $translate.use("en");
+                $.jStorage.set("language", "en");
             }
-            //  $rootScope.$apply();
-        };
+        }
+        //  $rootScope.$apply();
+    };
 
 
-    })
+})
 
-    ;
+;
